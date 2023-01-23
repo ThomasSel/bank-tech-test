bff8ef038d6fc7befa317e7a5d0a3b8dad4bf2b9
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-23 13:37:52 +0000

    Add tests for two deposits

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,6 +14,7 @@ describe Account do
       it "has a deposit of 1000 on 10/01/2023" do
         account = Account.new()
         account.deposit(1000, "2023-01-10")
+
         expect(account.history.first).to include(
           type: :deposit,
           amount: 1000.0,
@@ -24,6 +25,29 @@ describe Account do
       it "has a deposit of 99.99 on 15/01/2023" do
         account = Account.new()
         account.deposit(99.99, "2023-01-15")
+
+        expect(account.history.first).to include(
+          type: :deposit,
+          amount: 99.99,
+          date: Date.new(2023, 1, 15)
+        )
+      end
+    end
+
+    context "Two deposits" do
+      it "has two deposits" do
+        account = Account.new()
+        account.deposit(1000, "2023-01-10")
+        account.deposit(99.99, "2023-01-15")
+
+        expect(account.history.length).to eq(2)
+      end
+
+      it "has deposits ordered as they were entered" do
+        account = Account.new()
+        account.deposit(99.99, "2023-01-15")
+        account.deposit(1000, "2023-01-10")
+
         expect(account.history.first).to include(
           type: :deposit,
           amount: 99.99,
