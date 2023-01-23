e20c146e6cc707a81eea0adbe88c47be95279bd8
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-23 17:42:12 +0000

    Refactor tests using before(:each)

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,9 +2,13 @@ require("date")
 require("account")
 
 describe Account do
+  account = ""
+  before(:each) do
+    account = Account.new()
+  end
+
   context "No transactions" do
     it "has no transaction information" do
-      account = Account.new()
       expect(account.history).to eq([])
     end
   end
@@ -12,7 +16,6 @@ describe Account do
   describe "#deposit" do
     context "One deposit" do
       it "has a deposit of 1000 on 10/01/2023" do
-        account = Account.new()
         account.deposit(1000, "2023-01-10")
 
         expect(account.history.first).to include(
@@ -24,7 +27,6 @@ describe Account do
       end
 
       it "has a deposit of 99.99 on 15/01/2023" do
-        account = Account.new()
         account.deposit(99.99, "2023-01-15")
 
         expect(account.history.first).to include(
@@ -38,7 +40,6 @@ describe Account do
 
     context "Two deposits" do
       it "has two deposits" do
-        account = Account.new()
         account.deposit(1000, "2023-01-10")
         account.deposit(99.99, "2023-01-15")
 
@@ -46,7 +47,6 @@ describe Account do
       end
 
       it "adds up the deposits in the balance" do
-        account = Account.new()
         account.deposit(1000, "2023-01-10")
         account.deposit(99.99, "2023-01-15")
 
@@ -54,7 +54,6 @@ describe Account do
       end
 
       it "deposit dates must be chronological" do
-        account = Account.new()
         account.deposit(99.99, "2023-01-15")
         expect{ account.deposit(1000, "2023-01-10") }.to raise_error(
           "Transactions must be input chronologically"
@@ -62,7 +61,6 @@ describe Account do
       end
 
       it "deposits on the same day are stored in the order they were input" do
-        account = Account.new()
         account.deposit(1000, "2023-01-10")
         account.deposit(99.99, "2023-01-10")
 
@@ -74,7 +72,6 @@ describe Account do
   describe "#withdraw" do
     context "empty account" do
       it "raises when withdrawing" do
-        account = Account.new()
         expect{ account.withdraw(10, "2023-01-10") }.to raise_error(
           "You cannot withdraw 10.00 from your account, your current balance is 0.00"
         )
@@ -82,11 +79,13 @@ describe Account do
     end
 
     context "account with a balance of 100.00" do
+      before(:each) do
+        account.deposit(90, "2023-01-01")
+        account.deposit(10, "2023-01-02")
+      end
+
       context "one withdrawl" do
         it "can withdraw 40" do
-          account = Account.new()
-          account.deposit(90, "2023-01-01")
-          account.deposit(10, "2023-01-02")
           account.withdraw(40, "2023-01-10")
 
           expect(account.history.last).to include(
@@ -97,9 +96,6 @@ describe Account do
         end
 
         it "can withdraw 49.99" do
-          account = Account.new()
-          account.deposit(90, "2023-01-01")
-          account.deposit(10, "2023-01-02")
           account.withdraw(49.99, "2023-01-10")
 
           expect(account.history.last).to include(
@@ -110,9 +106,6 @@ describe Account do
         end
 
         it "raises when withdrawing more than 100" do
-          account = Account.new()
-          account.deposit(100, "2023-01-01")
-          
           expect{ account.withdraw(150, "2023-01-10") }.to raise_error(
             "You cannot withdraw 150.00 from your account, your current balance is 100.00"
           )
@@ -122,9 +115,6 @@ describe Account do
         end
 
         it "raises if date isn't after deposit" do
-          account = Account.new()
-          account.deposit(90, "2023-01-01")
-          account.deposit(10, "2023-01-02")
           expect{ account.withdraw(49.99, "2023-01-01") }.to raise_error(
             "Transactions must be input chronologically"
           )
@@ -133,8 +123,6 @@ describe Account do
 
       context "two withdrawls" do
         it "raises when withdrawing 40, then 100" do
-          account = Account.new()
-          account.deposit(100, "2023-01-01")
           account.withdraw(40, "2023-01-10")
           expect{ account.withdraw(100, "2023-01-11") }.to raise_error(
             "You cannot withdraw 100.00 from your account, your current balance is 60.00"
@@ -142,17 +130,13 @@ describe Account do
         end
 
         it "adds two transactions" do
-          account = Account.new()
-          account.deposit(100, "2023-01-01")
           account.withdraw(20, "2023-01-02")
           account.withdraw(50, "2023-01-03")
 
-          expect(account.history.length).to eq(3)
+          expect(account.history.length).to eq(4)
         end
 
         it "updates the balance" do
-          account = Account.new()
-          account.deposit(100, "2023-01-01")
           account.withdraw(20, "2023-01-02")
           account.withdraw(50, "2023-01-03")
           
@@ -160,8 +144,6 @@ describe Account do
         end
 
         it "fails if the withdrawls aren't chronological" do
-          account = Account.new()
-          account.deposit(100, "2023-01-01")
           account.withdraw(50, "2023-01-03")
           expect{ account.withdraw(20, "2023-01-02") }.to raise_error(
             "Transactions must be input chronologically"
@@ -169,8 +151,6 @@ describe Account do
         end
 
         it "withdrawls on the same day are stored in the order they were input" do
-          account = Account.new()
-          account.deposit(100, "2023-01-01")
           account.withdraw(20, "2023-01-02")
           account.withdraw(50, "2023-01-02")
 
