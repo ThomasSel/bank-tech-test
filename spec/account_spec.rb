919aaa41025707f586ce7f0baa0398cd16a6478e
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-23 12:30:09 +0000

    Add a single deposit test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,11 +13,21 @@ describe Account do
     context "One deposit" do
       it "has a deposit of 1000 on 10/01/2023" do
         account = Account.new()
-        account.deposit(1000, "2023-01-10");
+        account.deposit(1000, "2023-01-10")
         expect(account.history.first).to include(
           type: :deposit,
           amount: 1000.0,
           date: Date.new(2023, 1, 10)
+        )
+      end
+
+      it "has a deposit of 99.99 on 15/01/2023" do
+        account = Account.new()
+        account.deposit(99.99, "2023-01-15")
+        expect(account.history.first).to include(
+          type: :deposit,
+          amount: 99.99,
+          date: Date.new(2023, 1, 15)
         )
       end
     end
