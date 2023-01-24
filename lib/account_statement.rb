a3ffcbc6032fc9ce10d3488e266d7ab324fe8985
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-24 12:05:43 +0000

    Use private method for transaction formatting

--- lib/account_statement.rb
+++ lib/account_statement.rb
@@ -9,12 +9,23 @@ class AccountStatement
     end
     statement_array = ["date || credit || debit || balance"]
     @account.history.reverse.each do |transaction|
-      statement_array << "%s || %.2f || || %.2f" % [
-        transaction[:date].to_s.split("-").reverse.join("/"),
-        transaction[:amount],
-        transaction[:balance]
-      ]
+      statement_array << format_transaction(transaction)
     end
     return statement_array.join("\n")
+  end
+
+  private
+
+  def format_date(date)
+    date_string = date.to_s
+    return date_string.split("-").reverse.join("/")
+  end
+
+  def format_transaction(transaction)
+    return "%s || %.2f || || %.2f" % [
+      format_date(transaction[:date]),
+      transaction[:amount],
+      transaction[:balance]
+    ]
   end
 end
