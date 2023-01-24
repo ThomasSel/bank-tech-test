63db531d6b22c5274ce82c5673f9b3989764a34f
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-24 14:16:28 +0000

    Add tests for multiple transactions

--- spec/account_statement_spec.rb
+++ spec/account_statement_spec.rb
@@ -59,5 +59,42 @@ describe AccountStatement do
         )
       end
     end
+
+    context "with one deposit and one withdrawl" do
+      before(:each) do
+        allow(account).to receive(:history).and_return([
+          {
+            type: :deposit,
+            amount: 150,
+            date: Date.new(2023, 1, 1),
+            balance: 150
+          }, {
+            type: :withdrawl,
+            amount: 100,
+            date: Date.new(2023, 1, 2),
+            balance: 50
+          }
+        ])
+      end
+
+      it "returns a string with a header" do
+        expect(account_statement.get_statement).to include(
+          "date || credit || debit || balance"
+        )
+      end
+
+      it "returns a string with transactions in reverse order" do
+        expect(account_statement.get_statement).to match(
+          %r{02/01/2023.*\n01/01/2023}
+        )
+      end
+
+      it "correctly formats the output" do
+        expect(account_statement.get_statement).to include(
+          "02/01/2023 || || 100.00 || 50.00",
+          "01/01/2023 || 150.00 || || 150.00"
+        )
+      end
+    end
   end
 end
